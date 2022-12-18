99b1a49d6782211091142387a337d0fd24410809
duellal <alexandriaduell@gmail.com>
2022-12-18 00:33:14 -0500

    Completed

--- api/shippers/shippers-middleware.js
+++ api/shippers/shippers-middleware.js
@@ -1,12 +1,30 @@
-module.exports = {
-  checkId,
-  checkPayload,
-}
+const db = require(`../../data/db-config`)
 
-function checkId(req, res, next) {
+
+
+async function checkId(req, res, next) {
+  const shipper = await db(`shippers`).where(`shipperid`, req.params.id).first()
+
+  if (!shipper) {
+    next({
+      status: 404,
+      message: `the id of ${req.params.id} does not exist`
+    })
+  }
   next()
 }
 
 function checkPayload(req, res, next) {
-  next()
+  if (!req.body.Phone || !req.body.ShipperName) {
+    next({
+      status: 422,
+      message: `Phone and ShipperName are required.`
+    })
+  } else { next() }
+
+}
+
+module.exports = {
+  checkId,
+  checkPayload,
 }
